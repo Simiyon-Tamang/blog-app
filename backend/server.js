64bfaeea0b444c2388085eababe35a55d18bbbb0
Simiyon Fyuba Tamang <st276419@my.ncwu.edu>
2024-11-24 22:41:51 -0500

    mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
