068a55791c3a268ff58b9adae1423280d5aa15d6
Simiyon Fyuba Tamang <st276419@my.ncwu.edu>
2024-11-27 15:46:59 -0500

    SignUp, Login and Logout Api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,27 +2,11 @@ import User from "../models/user.model.js";
 import bycrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const login = async (req, res) => {
-  res.send("Login route");
-
-  const { userName, password } = req.body;
-
-  try {
-    if ((!userName, !password)) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
 export const signup = async (req, res) => {
-  res.send("Signup route");
-
-  const { fullName, userName, password } = req.body;
+  const { fullName, userName, password, confirmPassword, gender } = req.body;
 
   try {
-    if ((!fullName, !userName, !password)) {
+    if (!fullName || !userName || !password || !confirmPassword) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
@@ -31,39 +15,72 @@ export const signup = async (req, res) => {
         .status(400)
         .json({ message: "Password must be at least 8 characters" });
     }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
 
     const user = await User.findOne({ userName });
 
+    if (user) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
     const salt = await bycrypt.genSalt(10);
     const hashedPassword = await bycrypt.hash(password, salt);
-
-    if (user)
-      return res
-        .status(400)
-        .status.json({ message: "Username already exists" });
 
     const newUser = new User({
       fullName,
       userName,
       password: hashedPassword,
+      gender,
     });
 
     if (newUser) {
-      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
+      generateTokenAndSetCookie(newUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         userName: newUser.userName,
-        profilePic: newUser.profilePic,
+        // profilePic: newUser.profilePic,
       });
     } else {
       res.status(400).json({ message: "Internal Server Error" });
     }
-  } catch (error) {}
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    if (!userName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ userName });
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      // profilePic: user.profilePic,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout route");
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in Logout controller");
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,17 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
