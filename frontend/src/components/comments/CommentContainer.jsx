241fa96a007d8bfb3dc77c02f23626d404299064
Simiyon Fyuba Tamang <st276419@my.ncwu.edu>
2024-12-20 14:37:57 -0500

    UI updates for comment section

--- frontend/src/components/comments/CommentContainer.jsx
+++ frontend/src/components/comments/CommentContainer.jsx
@@ -23,15 +23,30 @@ const CommentContainer = ({ postId }) => {
     fetchComments();
   }, [postId]);
   return (
-    <div className="flex flex-col my-3 justify-center items-center">
+    <div className="flex flex-col my-3 justify-center items-center ">
       <CommentInput />
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col py-2 items-center justify-center">
         {loading ? (
           <div>Loading comments...</div>
         ) : (
           comments.map((comment) => (
-            <div key={comment._id} className="border p-2 my-2 w-[700px]">
-              <p>{comment.content}</p>
+            <div key={comment._id} className="flex space-x-3 my-2 ">
+              <div className="flex w-10 items-center justify-center">
+                <img
+                  className="w-10 h-10 rounded-full shadow-sm"
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+              <div className="border rounded-lg shadow-md p-2 my-3 w-[700px]">
+                <h3 className="font-bold text-lg text-blue-400">
+                  {comment.author.fullName}
+                </h3>
+                <div className="flex justify-between">
+                  <p className="text-sm">{comment.content}</p>
+                  <p className="text-xs text-gray-400">{comment.createdAt}</p>
+                </div>
+              </div>
             </div>
           ))
         )}
